0a6f194437bd67fe3db6539e6dc4b98758539fcf
ushabanda <122169198+ushabanda@users.noreply.github.com>
2023-05-19 18:44:59 +0530

    Add files via upload
    displaying hello world message in python

---
+++ test.py
@@ -0,0 +1,11 @@
+print("Hello world")
+# This is a comment
+
+'''
+Learning python from youtube by refering
+code with harry youtube channel
+python is trending programming language
+'''
+print("do not stop learning \'python\'")
+
+print("usha", 5, 9, 6 , sep="*", end="darshu")
