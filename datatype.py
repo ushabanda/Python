1ca506b34038480c532791f5e782d5bccd36fe56
ushabanda <122169198+ushabanda@users.noreply.github.com>
2023-05-23 18:57:04 +0530

    Add files via upload
    learning python datatypes

---
+++ datatype.py
@@ -0,0 +1,31 @@
+#float type
+x=2.5
+print(x)
+print(type(x))
+
+#complex type
+a=1j + 2j
+print(a)
+print(type(a))
+
+#list type
+b=["React","Python","SQL"]
+print(b)
+print(type(b))
+
+#tuple type
+c=('HTML','CSS','Javascript')
+print(c)
+print(type(c))
+
+#range type
+d=range(1,8)
+print(d)
+print(type(d))
+
+#dictionary type
+e={ 
+    "name":'usha',
+    "course":'MERN'}
+print(e)
+print(type(e))
